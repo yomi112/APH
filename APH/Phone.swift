1ff0bd344a8bd3ff9082577fabcfe84e899e80da
yomi112 <pauleremin93@gmail.com>
2021-11-22 22:14:44 +0300

    CS50 project Apple Phones Hardware

---
+++ APH/Phone.swift
@@ -0,0 +1,20 @@
+//
+//  Phones.swift
+//  APH
+//
+//  Created by robotyomi on 22.11.2021.
+//
+
+import Foundation
+
+
+struct Phone {
+    let name: String
+    let model: Int
+    let color: String
+    let memory: Int
+    let DisplaySize: Int
+    let GWave: Int
+    let ChipModel: Int
+    
+}
--- APH/ViewController.swift
+++ APH/ViewController.swift
@@ -7,13 +7,22 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+class ViewController: UITableViewController {
+    let phone = [
+        Phone(name: "Iphone 4", model: 4, color: "Black", memory: 4, DisplaySize: 4, GWave: 3, ChipModel: 3),
+        Phone(name: "Iphone 5", model: 5, color: "Black", memory: 16, DisplaySize: 5, GWave: 4, ChipModel: 5),
+        Phone(name: "Iphone 6", model: 6, color: "Gold", memory: 32, DisplaySize: 5, GWave: 4, ChipModel: 6)
+    ]
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
     }
-
-
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return phone.count
+    }
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell =  tableView.dequeueReusableCell(withIdentifier: "PhoneCell", for: indexPath)
+        cell.textLabel?.text = phone[indexPath.row].name
+        return cell
+    }
 }
 
